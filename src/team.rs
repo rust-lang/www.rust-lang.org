2ab84841c5f8f2c476d49d1f677021b69d96f847
Ashley Williams <ashley666ashley@gmail.com>
2018-11-17 14:56:37 -0500

    jfc

--- src/team.rs
+++ src/team.rs
@@ -2,11 +2,22 @@ extern crate serde_yaml;
 
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize)]
 pub struct Team {
     name: String,
+    description: String,
+    chat: String,
+    leads: Vec<String>,
+}
+
+fn read_team_yaml(team: &str) -> io::Result<Team> {
+    let data_path = Path::new("./src/data/teams/").join(team).join("team.yml");
+    assert!(fs::metadata(&data_path)?.is_file());
+    let data_string = fs::read_to_string(&data_path)?;
+    let data: Team = serde_yaml::from_str(&data_string).unwrap();
+    Ok(data)
 }
 
 pub fn get_teams() -> io::Result<Vec<Team>> {
@@ -14,20 +25,18 @@ pub fn get_teams() -> io::Result<Vec<Team>> {
     let team_data = fs::read_dir("./src/data/teams/")?;
     for team in team_data {
         let team = team?;
-        teams.push(Team {
-            name: team.file_name().into_string().unwrap(),
-        });
+        let data = read_team_yaml(&get_name_from_direntry(team.path())).unwrap();
+        teams.push(data);
     }
     Ok(teams)
 }
 
+fn get_name_from_direntry(path: PathBuf) -> String {
+  path.file_stem().unwrap().to_str().unwrap().to_string()
+}
+
 pub fn get_teaminfo(team: &str) -> io::Result<Team> {
-    println!("team {}", team);
-    let data_path = Path::new("./src/data/teams/").join(team).join("team.yml");
-    assert!(fs::metadata(&data_path)?.is_file());
-    let data_string = fs::read_to_string(&data_path)?;
-    let data: Team = serde_yaml::from_str(&data_string).unwrap();
-    Ok(data)
+    read_team_yaml(team)
 }
 
 pub fn get_subteams(team: &str) -> io::Result<Vec<Team>> {
@@ -38,9 +47,8 @@ pub fn get_subteams(team: &str) -> io::Result<Vec<Team>> {
         let team_data = fs::read_dir(data_path)?;
         for team in team_data {
             let team = team?;
-            teams.push(Team {
-                name: team.file_name().into_string().unwrap(),
-            });
+            let data = read_team_yaml(&get_name_from_direntry(team.path())).unwrap();
+            teams.push(data);
         }
     }
     Ok(teams)
@@ -53,9 +61,8 @@ pub fn get_subwgs(team: &str) -> io::Result<Vec<Team>> {
         let wg_data = fs::read_dir(data_path)?;
         for wg in wg_data {
             let wg = wg?;
-            wgs.push(Team {
-                name: wg.file_name().into_string().unwrap(),
-            });
+            let data = read_team_yaml(&get_name_from_direntry(wg.path())).unwrap();
+            wgs.push(data);
         }
     }
     Ok(wgs)
@@ -66,9 +73,8 @@ pub fn get_wgs() -> io::Result<Vec<Team>> {
     let team_data = fs::read_dir("./src/data/wgs/")?;
     for team in team_data {
         let team = team?;
-        teams.push(Team {
-            name: team.file_name().into_string().unwrap(),
-        });
+        let data = read_team_yaml(&get_name_from_direntry(team.path())).unwrap();
+        teams.push(data);
     }
     Ok(teams)
 }
