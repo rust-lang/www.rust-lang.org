d6a1c822545ea3faa98303e0287d7a5b0be25d9d
Phil Best <me@phillip.best>
2019-05-20 18:43:00 +0200

    Adds Skeleton vedor code back into main.rs. Adds custom CSS to correct issues with misaligned buttons in the get started with rust section. Adds several new Tachyons classes to fix alignment issues between links and labels in the Grow with Rust section.

--- src/main.rs
+++ src/main.rs
@@ -280,7 +280,7 @@ fn concat_vendor_css(files: Vec<&str>) {
 fn main() {
     compile_sass("app");
     compile_sass("fonts");
-    concat_vendor_css(vec!["tachyons"]);
+    concat_vendor_css(vec!["skeleton", "tachyons"]);
 
     rocket::ignite()
         .attach(Template::fairing())
