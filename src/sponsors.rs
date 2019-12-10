b0fd24e54c19c1e5c9823789ae479e55401e97ee
Pietro Albini <pietro@pietroalbini.org>
2019-12-10 11:35:29 +0100

    sponsors: dynamically render the page

---
+++ src/sponsors.rs
@@ -0,0 +1,44 @@
+use crate::i18n::I18NHelper;
+use std::error::Error;
+
+static SPONSORS_YML_PATH: &str = "src/data/sponsors.yml";
+
+lazy_static! {
+    static ref SPONSORS: Vec<Sponsor> = load_sponsors(SPONSORS_YML_PATH).unwrap();
+}
+
+#[derive(Deserialize)]
+struct Sponsor {
+    id: String,
+    name: String,
+}
+
+fn load_sponsors(path: &str) -> Result<Vec<Sponsor>, Box<dyn Error>> {
+    Ok(serde_yaml::from_str(&std::fs::read_to_string(path)?)?)
+}
+
+#[derive(Serialize)]
+pub(crate) struct RenderSponsor {
+    name: &'static str,
+    is_not_first: bool,
+    logo_path: String,
+    logo_alt_i18n: String,
+    description_i18n: String,
+}
+
+pub(crate) fn render_data(lang: &str) -> Vec<RenderSponsor> {
+    let i18n = I18NHelper::new();
+
+    let sponsors = SPONSORS
+        .iter()
+        .enumerate()
+        .map(|(i, s)| RenderSponsor {
+            name: &s.name,
+            is_not_first: i != 0,
+            logo_path: format!("/static/images/sponsor-logos/{}.png", s.id),
+            logo_alt_i18n: i18n.lookup(lang, &format!("sponsors-{}-alt", s.id), None),
+            description_i18n: i18n.lookup(lang, &format!("sponsors-{}", s.id), None),
+        })
+        .collect::<Vec<_>>();
+    sponsors
+}
