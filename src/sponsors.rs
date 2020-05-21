cd7e69275fdab1afb7704416823a81992021338d
Pietro Albini <pietro@pietroalbini.org>
2020-05-21 14:57:48 +0200

    add google cloud to the list of sponsors

--- src/sponsors.rs
+++ src/sponsors.rs
@@ -10,9 +10,13 @@ lazy_static! {
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
 struct Sponsor {
     id: String,
     name: String,
+    logo_format: Option<String>,
+    #[serde(default)]
+    vertical_logo: bool,
 }
 
 fn load_sponsors(path: &str) -> Result<Vec<Sponsor>, Box<dyn Error>> {
@@ -26,6 +30,7 @@ pub(crate) struct RenderSponsor {
     logo_path: String,
     logo_alt_i18n: String,
     description_i18n: String,
+    vertical_logo: bool,
 }
 
 pub(crate) fn render_data(lang: &str) -> Vec<RenderSponsor> {
@@ -36,9 +41,14 @@ pub(crate) fn render_data(lang: &str) -> Vec<RenderSponsor> {
         .map(|s| RenderSponsor {
             name: &s.name,
             is_not_first: true, // Will be changed later
-            logo_path: format!("/static/images/sponsor-logos/{}.svg", s.id),
+            logo_path: format!(
+                "/static/images/sponsor-logos/{}.{}",
+                s.id,
+                s.logo_format.as_ref().map(|s| s.as_str()).unwrap_or("svg")
+            ),
             logo_alt_i18n: i18n.lookup(lang, &format!("sponsors-{}-alt", s.id), None),
             description_i18n: i18n.lookup(lang, &format!("sponsors-{}", s.id), None),
+            vertical_logo: s.vertical_logo,
         })
         .collect::<Vec<_>>();
 
