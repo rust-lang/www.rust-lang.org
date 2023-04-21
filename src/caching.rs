f7c3f1dfa17d7f959b61b0ff3eb941968cbdb76e
Jan David <jdno@jdno.dev>
2023-04-21 12:41:56 +0200

    Fix Cache-Control header

--- src/caching.rs
+++ src/caching.rs
@@ -1,6 +1,5 @@
 use rocket::fs::NamedFile;
-use rocket::http::uncased::Uncased;
-use rocket::http::Header;
+use rocket::http::{hyper, Header};
 use rocket::response::Responder;
 
 #[derive(Responder)]
@@ -13,12 +12,10 @@ impl CachedNamedFile {
     pub fn max_age(file: NamedFile, max_age: u32) -> Self {
         Self {
             file,
-            header: Header {
-                name: Uncased {
-                    string: "CacheControl".into(),
-                },
-                value: format!("max-age={max_age}").into(),
-            },
+            header: Header::new(
+                hyper::header::CACHE_CONTROL.as_str(),
+                format!("max-age={max_age}"),
+            ),
         }
     }
 }
